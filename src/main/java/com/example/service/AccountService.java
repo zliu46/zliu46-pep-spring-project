b633cad53d9f1a4cac6cf56aa1a9fba9cf8107e4
Zhou Liu <zhouliu2111@gmail.com>
2025-06-27 18:57:29 +0000

    Added AccountRepo vaiable, added register and login methods to AccountService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,21 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account register(Account account) {
+        return null;
+    }
+
+    public Account login(Account account) {
+        return null;
+    }
 }
