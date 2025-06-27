bb8443e23924157b2ebff43b47ce268721daadae
Zhou Liu <zhouliu2111@gmail.com>
2025-06-27 19:27:42 +0000

    Added Service properties and http methods to SocialMediaController

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +20,52 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Account account) {
+        
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Account account) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/messages") 
+    public ResponseEntity<?> createMessage(@RequestBody Message message) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<?> getAllMessages() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/messages/{messageId}")
+    public ResponseEntity<?> getMessageById(@PathVariable Integer messageId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/messages/{messageId}")
+    public ResponseEntity<?> deleteMessage (@PathVariable Integer messageId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/messages/{messageId}")
+    public ResponseEntity<?> updateMessage(@PathVariable Integer messageId, @RequestBody Message newText) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/accounts/{accountId}/messages") 
+    public ResponseEntity<?> getMessageByAccountId(@PathVariable Integer accountId) {
+        return ResponseEntity.ok().build();
+    }
 }
