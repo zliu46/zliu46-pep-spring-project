d82990b68d4179599451f803525e9b017daa14f5
Zhou Liu <zhouliu2111@gmail.com>
2025-06-27 19:04:40 +0000

    Added Message Repository and methods to MessageService

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,46 @@
 package com.example.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message createMessage(Message message) {
+        return null;
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageById(Integer id) {
+        return messageRepository.findById(id).orElse(null);
+    }
+
+    public int deleteMessage(Integer id) {
+        if (messageRepository.existsById(id)) {
+            messageRepository.deleteById(id);
+            return 1;
+        }
+        return 0;
+    }
+
+    public int updateMessage(Integer id, String newText) {
+        Message currentMessage = getMessageById(id);
+
+        if (currentMessage != null) {
+            currentMessage.setMessageText(newText);
+            return 1;
+        }
+
+        return 0;
+    }
+
 }
