5b4101b7cc93e7fefff0b136d9a6113048772d6d
Zhou Liu <zhouliu2111@gmail.com>
2025-06-28 10:19:32 +0000

    Imported AuthenticationFailedException to AccountService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.entity.Account;
+import com.example.exception.AuthenticationFailedException;
 import com.example.exception.InvalidAccountDataException;
 import com.example.exception.UsernameAlreadyExistsException;
 import com.example.repository.AccountRepository;
