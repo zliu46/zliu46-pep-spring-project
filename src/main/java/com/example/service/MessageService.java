b911e461b71515f0806f026bd7091ca188922a1e
Zhou Liu <zhouliu2111@gmail.com>
2025-06-28 10:27:58 +0000

    Added AccountRepository to MessageService

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -3,14 +3,17 @@ package com.example.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.entity.Message;
+import com.example.repository.AccountRepository;
 import com.example.repository.MessageRepository;
 
 @Service
 public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     public Message createMessage(Message message) {
         return null;
