b72b54d1a3146590c658009f7787283ab5043c13
Zhou Liu <zhouliu2111@gmail.com>
2025-06-28 09:42:04 +0000

    Created UsernameAlreadyExistsException

---
+++ src/main/java/com/example/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException (String message) {
+        super(message);
+    }
+}
