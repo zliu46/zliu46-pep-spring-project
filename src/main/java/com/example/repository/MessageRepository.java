46190e182271494c0427ef21e2f4bd45723d26de
Zhou Liu <zhouliu2111@gmail.com>
2025-06-28 11:59:26 +0000

    Updated MessageRepository to add findByPostedBy method

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,8 +1,11 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Integer>{
+    List<Message> findByPostedBy(Integer postedby);
 }
